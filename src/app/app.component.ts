356908861d4fc0ec783b7e0a6004c63f9d47c918
Mr. Yugen <mryugenmystery@gmail.com>
2025-09-24 14:07:51 +0200

    Añadido sitemap.xml y robots.txt. Se implementa un CMP (Consent Management Platform) que muestre un banner de cookies, recuerde la preferencia del usuario y bloquee Analytics hasta obtener consentimiento.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';  // Importamos el Router de Angular
+import { filter } from 'rxjs/operators';                  // Importamos 'filter' para filtrar eventos
+declare let gtag: Function;  // Declara la función gtag para que TypeScript la reconozca
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mr-yugen-web';
+
+  constructor(private router: Router) {
+    // Suscribirse a eventos de finalización de navegación
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e) => {
+        if (typeof gtag !== 'undefined') {
+          gtag('event', 'page_view', {
+            page_path: e.urlAfterRedirects,
+            page_title: document.title,
+            page_location: window.location.href, // <- recomendado por Google
+          });
+        }
+      });
+    }
 }
+
